c70c9218518c442e7ca35245775f065e4a717633
JhonFajardoRodas <jj.fajardo17@hotmail.com>
2016-04-04 14:48:33 +0200

    terminado

--- ejercicio_5-6.5/ajax.js
+++ ejercicio_5-6.5/ajax.js
@@ -3,8 +3,16 @@ $(document).ready(function() {
     url: 'docx.txt',
     cache: false
   }).done(function(data){
-    $('#first').click(function(){
-      $('#second').html(data);
+    $('#second').html(data);
+    $('#second').click(function(){
+
+      $.ajax({
+        url:'docx2.txt',
+        cache: false
+      }).done(function(data){
+          $('#second').append(data);
+      });
+
     });
   }).fail(function(){
     alert("error: document not found")
